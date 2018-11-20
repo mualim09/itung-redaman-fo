7d83c0f68bb9591476ce7539a80cbfe651af81ec
rdsurya147@gmail.com <rdsurya147@gmail.com>
2018-11-20 23:00:39 +0800

    Connector ready for execute and retrieve

---
+++ src/java/dbcon/MyConnector.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbcon;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException; 
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MyConnector {
+    private static final String dbname = "tsm_infocentre";
+    private static final String username = "root";
+    private static final String password = "qwerty";
+    
+    public Connection conn;
+    
+    public MyConnector(){
+        conn = null;
+    }
+
+    public static void main(String[] args) {
+        
+     
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+dbname+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+    }
+    
+    public boolean createConnection(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return false;
+        }
+        
+         try {
+            conn = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+dbname+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return false;
+        }
+        
+         if (conn == null) {
+            System.out.println("Failed to make connection!");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+    
+    public void closeConnection(){
+        try{
+            conn.close();
+        }catch(Exception e){
+        
+        }
+    }
+    
+    public boolean executeQuery(String query){
+        Statement statement = null;
+        boolean isSuccess = true;
+        
+        try{
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+        }catch(Exception e){
+            e.printStackTrace();
+            isSuccess = false;
+        }finally{
+            try{
+                statement.close();
+            }catch(Exception e){
+                
+            }
+        }
+        
+        return isSuccess;
+    }
+    
+    public ArrayList<ArrayList<String>> getData(String query){
+        
+        ArrayList<ArrayList<String>> records = new ArrayList<ArrayList<String>>();
+        
+        Statement statement = null;
+        ResultSet rs = null;
+        ResultSetMetaData rsmd = null;
+        
+        try{
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            rsmd = rs.getMetaData();
+            
+            int columnsNumber = rsmd.getColumnCount();
+            
+            while(rs.next()){
+                ArrayList<String> column = new ArrayList<String>();
+                for(int i = 1; i <= columnsNumber; i++){
+                    column.add(rs.getString(i));
+                }
+                
+                records.add(column);
+            }
+        
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            try{ rs.close();}catch(Exception e){}
+            try{ statement.close();}catch(Exception e){}            
+        }
+        
+        
+        return records;
+    
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,48 @@
+<%-- 
+    Document   : index
+    Created on : Nov 20, 2018, 10:01:57 PM
+    Author     : Lenovo
+--%>
+
+<%@page import="java.util.ArrayList"%>
+<%@page import="dbcon.MyConnector"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    MyConnector myCon = new MyConnector();
+    if (!myCon.createConnection()) {
+        out.print("Failed to connect");
+        return;
+    }
+    
+    //String query="insert INTO adm_user(nik, name, password, created_date) values ('940146', 'Firman Aden', 'Monyet23', now());";
+    String query="Select * from adm_user;;";
+    /*if(!myCon.executeQuery(query)){
+        out.print("Query fail!");
+        return;
+    }
+    */
+    
+    ArrayList<ArrayList<String>> arrData = myCon.getData(query);
+    
+    for(ArrayList<String> row : arrData){
+        String line = "";
+        for(String col : row){
+            line+=col+"  ";
+        }
+        
+        out.print(line + "<br>");
+    }
+    
+    myCon.closeConnection();
+
+%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
